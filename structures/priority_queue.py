8e2f7a46536e65b81f2286be8146db79f49b18cf
v1shnev11ble1zer <oleg999000@yandex.ru>
2021-08-23 23:52:43 +0300

    init commit

--- structures/priority_queue.py
+++ structures/priority_queue.py
@@ -1,23 +1,3 @@
-class PriorityQueue:
-
-    def push(self, key, value):
-        pass
-
-    def pop(self):
-        pass
-
-    def get(self):
-        pass
-
-    @staticmethod
-    def parent(i):
-        return (i - 1) // 2
-
-    @staticmethod
-    def left(i):
-        return 2 * i + 1
-
-    @staticmethod
-    def right(i):
-        return 2 * i + 2
+class PriorityMinQueue:
+    pass
 

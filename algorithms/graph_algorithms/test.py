ef0ac8affc658e3168db50b8458367f6501b3b4c
v1shnev11ble1zer <oleg999000@yandex.ru>
2021-08-29 19:41:39 +0300

    add breath_first_search for graph_algorithms

--- algorithms/graph_algorithms/test.py
+++ algorithms/graph_algorithms/test.py
@@ -8,7 +8,7 @@ class GraphTestCase(unittest.TestCase):
         self.graph = {'0': ['1', '2'], '1': [], '2': ['3'], '3': ['4', '5', '6'], '4': [], '5': ['7', '8'], '6': ['9'],
                       '7': [], '8': [], '9': []}
 
-    def test_something(self):
+    def test_bfs(self):
         self.assertEqual(bfs(self.graph, '0', '0'), ['0'])
         self.assertEqual(bfs(self.graph, '0', '9'), ['0', '2', '3', '6', '9'])
         self.assertEqual(bfs(self.graph, '0', '7'), ['0', '2', '3', '5', '7'])
